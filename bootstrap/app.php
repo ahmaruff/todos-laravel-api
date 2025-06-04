6989005eddc6a3401c2bf24455d60386e2cc310f
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-06-04 21:49:27 +0700

    feat: add log middleware to handle request/response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\App\Http\Middleware\RequestLoggingMiddleware::class);
+
+        $middleware->alias([
+            'log-requests' => \App\Http\Middleware\RequestLoggingMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
