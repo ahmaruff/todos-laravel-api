b0ce324f3e39197e8bbe5e6b384f6ba5874325ef
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-06-06 17:37:37 +0700

    fix: response error handling

--- app/Services/ExceptionHandlerService.php
+++ app/Services/ExceptionHandlerService.php
@@ -39,13 +39,20 @@ class ExceptionHandlerService
             return $this->renderMappedException($e, $this->exceptionMap[$exceptionClass]);
         }
 
+        $rawCode = (int) $e->getCode();
+        $code = ($rawCode >= 100 && $rawCode < 600) ? $rawCode : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        $status = $code >= 400 && $code < 500
+            ? ResponseJsonCommand::FAIL
+            : ResponseJsonCommand::ERROR;
+
         $default = [
-            'status' => LogService::STATUS_ERROR,
-            'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+            'status' => $status,
+            'code' => $code,
             'message' => $e->getMessage(),
             'log_level' => LogService::LEVEL_ERROR,
             'log_task' => 'default_exception_handling',
-            'log_status' => LogService::STATUS_ERROR,
+            'log_status' => $status,
         ];
 
         return $this->renderMappedException($e, $default);
@@ -116,103 +123,103 @@ class ExceptionHandlerService
     {
         $this->exceptionMap = [
             AccessDeniedHttpException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_FORBIDDEN,
                 'message' => "Whoops! Looks like you're not authorized to view this content. Please check with the admin if you think this is a mistake",
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'authorization_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             AuthenticationException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_UNAUTHORIZED,
                 'message' => 'Authentication required',
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'authentication_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             AuthorizationException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_FORBIDDEN,
                 'message' => "Whoops! You don't have permission to access this resource",
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'authorization_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             NotFoundHttpException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_NOT_FOUND,
                 'message' => "Whoops! Resource Unavailable: The API endpoint you're attempting to access doesn't exist",
-                'log_level' => 'warning',
+                'log_level' => LogService::LEVEL_WARNING,
                 'log_task' => 'not_found_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             QueryException::class => [
-                'status' => 'error',
+                'status' => ResponseJsonCommand::ERROR,
                 'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
                 'message' => "Whoops! It looks like there was a hiccup while executing your request. The database query encountered an error",
-                'log_level' => 'critical',
+                'log_level' => LogService::LEVEL_CRITICAL,
                 'log_task' => 'sql_query_exception_handling',
-                'log_status' => 'error'
+                'log_status' => LogService::STATUS_ERROR,
             ],
 
             PDOException::class => [
-                'status' => 'error',
+                'status' => ResponseJsonCommand::ERROR,
                 'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
                 'message' => "Whoops! PDOException. Check database settings and query syntax",
-                'log_level' => 'critical',
+                'log_level' => LogService::LEVEL_CRITICAL,
                 'log_task' => 'database_exception_handling',
-                'log_status' => 'error'
+                'log_status' => LogService::STATUS_ERROR,
             ],
 
             MethodNotAllowedHttpException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_METHOD_NOT_ALLOWED,
                 'message' => "Whoops! It seems you're using the wrong method here. This action isn't supported for this resource",
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'http_method_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             BadRequestException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_BAD_REQUEST,
                 'message' => 'Bad request',
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'bad_request_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             ModelNotFoundException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_NOT_FOUND,
                 'message' => "Whoops! It seems we couldn't find what you were looking for. The requested data couldn't be located",
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'model_not_found_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             ValidationException::class => [
-                'status' => 'fail',
+                'status' => ResponseJsonCommand::FAIL,
                 'code' => Response::HTTP_BAD_REQUEST,
                 'message' => "Whoops! It seems the data you entered didn't pass our validation checks. There might be some errors or missing fields",
-                'log_level' => 'info',
+                'log_level' => LogService::LEVEL_INFO,
                 'log_task' => 'validation_exception_handling',
-                'log_status' => 'fail'
+                'log_status' => LogService::STATUS_FAIL,
             ],
 
             // Default fallback
             'default' => [
-                'status' => 'error',
+                'status' => ResponseJsonCommand::ERROR,
                 'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
                 'message' => 'An unexpected error occurred',
                 'log_level' => 'error',
                 'log_task' => 'default_exception_handling',
-                'log_status' => 'error'
+                'log_status' => LogService::STATUS_ERROR,
             ]
         ];
     }
